89d0f00a0001211edb9c1b18e289cf0c327182a4
mammothgazillionaire <mammothgazillionaire@gmail.com>
2018-11-29 16:59:05 +0530

    contact form

--- Day 2/Day 2/contact/index.js
+++ Day 2/Day 2/contact/index.js
@@ -1,6 +1,9 @@
 const http = require('http');
 const fs = require('fs');
 const {parse} = require('querystring');
+var url = require('url');
+
+
 
 const port = 5000;
 const host = '127.0.0.1';
@@ -21,18 +24,13 @@ const server = http.createServer((request, response) =>{
         body = parse(body);
         console.log(body);
 
-        const data = `
-          Name = ${body.name}
-          Email = ${body.email}
-        `
-
-        fs.appendFile('data.txt', data ,  (err) => {
+        fs.appendFile('data.txt', ` ${JSON.stringify(body)} ` ,  (err) => {
           if (err) throw err;
           console.log('Saved!');
           
         });
-        response.end('info recieved');
     });
+    response.end('info recieved');
 }
 else if( request.method === "GET" && request.url === "/")
 {
@@ -43,6 +41,56 @@ else if( request.method === "GET" && request.url === "/")
       response.write(html);
       response.end();
     });
+  }
+  else if (request.method ==="GET" && request.url === "/list"){
+    fs.readFile('./data.txt', read = (err,data) => {
+      if(err) throw err;
+      response.writeHeader(200, {"Content-type":"text/plain"});
+      response.write(data);
+      response.end();
+    });
+  }else if(request.method ==="GET" && request.url === "/list/praveen"){
+    fs.readFile('./data.txt', read = (err,data) => {
+      var newData =  ''+ data;   
+      var splitData = newData.split(' ');
+      console.log(splitData);
+      var match = /praveen/i;
+      var newArr = splitData.filter(d => match.test(d));
+      console.log(newArr);
+      var parseData = JSON.parse(newArr[0]);
+      console.log(parseData);
+      let userStr = `Name: ${parseData.name}
+                     Email: ${parseData.email}`;
+      
+      if(err) throw err;
+      response.writeHeader(200, {"Content-type":"text/plain"});
+      response.write(userStr);
+      response.end();
+    });
+  }else if(request.method ==="GET" && request.url === request.url){
+    var q = url.parse(request.url,true);
+    var qData = q.query;
+    console.log(qData);
+    var username = qData.name
+    console.log(username);
+    
+    if(username) {
+      fs.readFile('./data.txt', read = (err,data) => {
+        var newData =  ''+ data;   
+        var splitData = newData.split(' ');
+        var match = new RegExp(username, 'i');
+        var newArr = splitData.filter(d => match.test(d));
+        var parseData = JSON.parse(newArr[0]);
+        let userStr = 
+        `Name: ${parseData.name}
+         Email: ${parseData.email}
+        `;
+        if(err) throw err;
+        response.writeHeader(200, {"Content-type":"text/plain"});
+        response.write(userStr);
+        response.end();
+      });
+    }
   }
 });
 
