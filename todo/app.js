c384a06a8faf2a520ffb703d182985ef1e1b51ae
mammothgazillionaire <mammothgazillionaire@gmail.com>
2018-12-03 22:59:03 +0530

    put delete methods implemented

--- todo/app.js
+++ todo/app.js
@@ -5,6 +5,8 @@ const fs = require("fs");
 const mongoose = require( 'mongoose' );
 const bodyParser = require('body-parser');
 const path = require('path');
+const methodOverride = require('./methodOverride')();
+// const methodOverride = require('method-override');
 
 mongoose.connect( 'mongodb://localhost/todo', (err,connection) => {
   if(err) throw err;
@@ -28,6 +30,38 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
+// middleware method Override
+
+app.use((req,res,next) => {
+  
+  switch(req.body._method){
+    case 'PUT':
+        req.method = 'PUT';
+        next(); 
+        break;       
+    case 'DELETE':
+      req.method = 'DELETE';
+      next();
+      break;
+    default:
+      return next();
+  }
+})
+
+
+// app.use(methodOverride(function (req, res) {
+//   if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+//     // look in urlencoded POST bodies and delete it
+//     console.log(req.body);
+//     var method = req.body._method
+//     console.log(method);
+//     delete req.body._method
+//     return method
+//   }
+// }));
+
+app.use(methodOverride('_method'));
+
 // logger
 app.use((req,res,next) => {
   let logger = `METHOD is ${req.method} PATH is ${req.url} created on ${new Date()}\n`;
@@ -38,11 +72,28 @@ app.use((req,res,next) => {
 })
 
 
+
 app.get('/',(req, res) => {
   Todo.find({},(err,data)=>{
     res.render('index', {todos:data});
   })
 });
+
+
+app.post('/',(req,res)=> {
+  console.log(req.body);
+  let item = req.body;
+  item.done = false;
+  let todo = new Todo(item);
+  console.log(todo);
+  
+
+  todo.save((err,data)=> {
+    console.log(data);
+    res.redirect('/')
+  })
+});
+
 
 app.get('/todos/:id',(req, res) => {
   Todo.find({_id: req.params.id},(err,todo)=>{
@@ -54,70 +105,31 @@ app.get('/todos/:id',(req, res) => {
 // update
 
 
-app.route("/todos/:id/edit").get((req, res) => {
-  Todo.find({_id : req.params.id}, (err, data) => {
-    res.render('edit', {todo : data});
-  })
-})
-.post((req, res) => {
+app.get("/todos/:id/edit",(req, res) => {
   const id = req.params.id;
-  console.log(id);
-  let data = req.body;
-  console.log(data, "<= data")
-  Todo.update({_id : id}, {$set : {...data}}, (err, data) => {
-    res.redirect('/')
-  })
-  
+  Todo.find({_id : id}, (err, data) => {
+    res.render('edit', {todo : data})
+  });
 });
 
-
-// app.route("/todos/:id/edit").get((req, res) => {
-//   res.render('edit');
-// }).put((req, res) => {
-//   // find data
-//   Todo.findByIdAndUpdate({ _id: req.params.id }, (err, todo) => {
-//     todo.todo = todo;
-//     todo.save((err, updatedData) => {
-//       res.render('edit',updatedData);
-//     });
-//   });
-// });
+app.put(("/todos/:id"),(req, res) => {
+  const id = req.params.id;
+  let data = req.body;
+  Todo.updateOne({ _id: id },{$set : {...data}},(err,data) =>{
+    if(err) throw err;
+    res.redirect('/');
+  })
+});
 
 // delete
 
-
-app.get("/todos/:id/delete",(req, res) => {
-  const id = req.params.id;
-  Todo.remove({_id : id}, (err) => {
-    res.redirect('/')
-  })
+app.delete("/todos/:id/delete",(req, res) => {
+  // find data
+  Todo.remove({ _id: req.params.id }, (err, todo) => {
+      res.redirect('/');
+  });
 });
 
-// app.delete("/todos/:id/delete",(req, res) => {
-//   // find data
-//   Todo.findByIdAndDelete({ _id: req.params.id }, (err, todo) => {
-//     todo.remove((err, updatedData) => {
-//       res.render('index',updatedData);
-//     });
-//   });
-// });
-
-app.post('/',(req,res)=> {
-  console.log(req.body);
-  
-  let item = req.body;
-  item.done = false;
-  let todo = new Todo(item);
-  // const { content, done } = req.body;
-  // let todo = new Todo({content, done});
-  console.log(todo);
-  
-
-  todo.save((err,data)=> {
-    console.log(data);
-    res.redirect('/')
-  })
-});
 
 // app.get('*',(req, res) => {
 //   res.render('<h2>Page Not Found</h2>');
---
+++ todo/methodOverride.js
@@ -0,0 +1,26 @@
+module.exports = function(key){
+  key = key || "_method";
+  return function methodOverride(req, res, next) {
+    var method;
+    req.originalMethod = req.originalMethod || req.method;
+    
+    // req.body
+    
+    if (req.body && typeof req.body === 'object' && key in req.body) {
+      method = req.body[key].toLowerCase();
+      delete req.body[key];
+    }
+    
+    // check X-HTTP-Method-Override
+    
+    if (req.headers['x-http-method-override']) {
+      method = req.headers['x-http-method-override'].toLowerCase();
+    }
+    
+    // replace
+    
+    if (supports(method)) req.method = method.toUpperCase();
+    next();
+  };
+};
+
